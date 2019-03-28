38e802b699515fd5db395639fb90f3a3900aa97c
JackTanRoo <jack.tan.lu@hotmail.com>
2019-03-27 23:07:21 -0700

    landing page working

--- server.js
+++ server.js
@@ -2,7 +2,7 @@ var express = require('express');
 var ejs = require('ejs');
 
 var app = express();
-var port = process.env.PORT || 3000;
+var port = process.env.PORT || 8080;
 
 
 app.set('views', __dirname + '/public');
